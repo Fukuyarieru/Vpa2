5185538e8c4f18d6ee98258d1b370e8e7cc4e4b2
Fukuya <fukuyarieru@gmail.com>
2025-07-07 21:15:29 +0300

    idea for getting data from db

--- app/src/main/java/com/vpa2/Database.java
+++ app/src/main/java/com/vpa2/Database.java
@@ -1,7 +1,14 @@
 package com.vpa2;
 
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.vpa2.datastructures.User;
 
 public abstract class Database {
@@ -10,5 +17,19 @@ public abstract class Database {
 
    public static<T extends DatabaseDatastructure> T get(String header, String key) {
       database.getReference(header).child(key);
+      return null; // someday on a sunny day
+   }
+   public static<T extends Class> T getObjectFromRefrence(DatabaseReference dbRef) {
+      dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+         @Override
+         public void onDataChange(@NonNull DataSnapshot snapshot) {
+            snapshot.getValue(T.class);
+         }
+
+         @Override
+         public void onCancelled(@NonNull DatabaseError error) {
+
+         }
+      });
    }
 }
