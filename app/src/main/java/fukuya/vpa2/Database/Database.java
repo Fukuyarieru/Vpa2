3df2a9ebe9ab2af20e522754e3617d838b8f4450
F.U <abc@email.com>
2025-09-01 22:43:59 +0300

    finished making get, also added the build.gradle

--- app/src/main/java/fukuya/vpa2/Database/Database.java
+++ app/src/main/java/fukuya/vpa2/Database/Database.java
@@ -8,29 +8,38 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Database {
-    private static FirebaseDatabase database=FirebaseDatabase.getInstance();
+    private static final FirebaseDatabase database=FirebaseDatabase.getInstance();
 
 //    public T get<T>() {}
 
-    public<T> T get(DatabaseLink<T> link) {
+    public<T> CompletableFuture<T> get(DatabaseLink<T> link) {
+        CompletableFuture<T> future=new CompletableFuture<>();
         DatabaseReference ref=this.getRef(link);
-        T var=null;
+
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                var=snapshot.getValue()
+                try {
+                    T value=snapshot.getValue(link.getClazz());
+                    future.complete(value);
+                } catch (Exception e) {
+                    future.completeExceptionally(e);
+                }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                future.completeExceptionally(error.toException());
             }
         });
+        return future;
     }
     public<T> DatabaseReference getRef(DatabaseLink<T> link) {
         DatabaseReference ref= database.getReference();
-        for(String str: link.path) {
+        for(String str: link.getPath()) {
             ref=ref.child(str);
         }
         return ref;
--- app/src/main/java/fukuya/vpa2/Database/DatabaseLink.java
+++ app/src/main/java/fukuya/vpa2/Database/DatabaseLink.java
@@ -3,9 +3,18 @@ package fukuya.vpa2.Database;
 import java.util.Collection;
 
 public class DatabaseLink<T> {
-    Collection<String> path;
+    private final Collection<String> path;
+    private final Class<T> clazz;
 
-    public DatabaseLink(Collection<String> path) {
+    public DatabaseLink(Collection<String> path, Class<T> clazz) {
         this.path=path;
+        this.clazz=clazz;
+    }
+
+    public Collection<String> getPath() {
+        return this.path;
+    }
+    public Class<T> getClazz() {
+        return clazz;
     }
 }
